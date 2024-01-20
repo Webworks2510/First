7a293fb8b0021bb9497ef81ad47fd6da79d6af2b
Saurabh Jain <sj2100333@gmail.com>
2024-01-20 15:38:50 +0530

    Done

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const mysql = require("mysql");
 
-
-app.get('/checking', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
-})
-app.get('/doublecheck', (req, res) => {
-  res.status(200).json('Welcome, your app is working well now also');
-})
-
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+const db = mysql.createConnection({
+	host: bn26fvsk0hn87edanoym - mysql.services.clever - cloud.com, // Replace with your database hostname
+	user: umgfjp60t6onhv3z, // Replace with your database username
+	password: oWzpM9hWP6f4OMT12B7J, // Replace with your database password
+	database: bn26fvsk0hn87edanoym, // Replace with your database name
 });
 
+app.get("/checking", (req, res) => {
+	res.status(200).json("Welcome, your app is working well");
+});
+app.get("/doublecheck", (req, res) => {
+	res.status(200).json("Welcome, your app is working well now also");
+});
+app.get("/user", (req, res) => {
+	const sql = "SELECT * FROM USER";
+	db.query(sql, (err, data) => {
+		if (err) res.json(err);
+		return res.json(data);
+	});
+});
 
-module.exports = app
+app.listen(PORT, () => {
+	console.log(`Server running at http://localhost:${PORT}`);
+});
+
+module.exports = app;
