586cb1248988bdd42ac03abb201b08abf58e6ea1
Saurabh Jain <sj2100333@gmail.com>
2024-01-20 15:44:17 +0530

    Done

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const PORT = 3000;
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-	host: bn26fvsk0hn87edanoym - mysql.services.clever - cloud.com, // Replace with your database hostname
-	user: umgfjp60t6onhv3z, // Replace with your database username
-	password: oWzpM9hWP6f4OMT12B7J, // Replace with your database password
-	database: bn26fvsk0hn87edanoym, // Replace with your database name
+	host: "bn26fvsk0hn87edanoym-mysql.services.clever-cloud.com", // Replace with your database hostname
+	user: "umgfjp60t6onhv3z", // Replace with your database username
+	password: "oWzpM9hWP6f4OMT12B7J", // Replace with your database password
+	database: "bn26fvsk0hn87edanoym", // Replace with your database name
 });
 
 app.get("/checking", (req, res) => {
@@ -17,9 +17,9 @@ app.get("/doublecheck", (req, res) => {
 	res.status(200).json("Welcome, your app is working well now also");
 });
 app.get("/user", (req, res) => {
-	const sql = "SELECT * FROM USER";
+	const sql = "SELECT * FROM user";
 	db.query(sql, (err, data) => {
-		if (err) res.json(err);
+		if (err) res.json(err+" Due To Select");
 		return res.json(data);
 	});
 });
