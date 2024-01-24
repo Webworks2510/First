70e9df0dcb545ed3149202a126a5d2f4bca9905c
Saurabh Jain <sj2100333@gmail.com>
2024-01-24 12:09:20 +0530

    changed to aws db

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const PORT = 3000;
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-	host: "bn26fvsk0hn87edanoym-mysql.services.clever-cloud.com", // Replace with your database hostname
-	user: "umgfjp60t6onhv3z", // Replace with your database username
-	password: "oWzpM9hWP6f4OMT12B7J", // Replace with your database password
-	database: "bn26fvsk0hn87edanoym", // Replace with your database name
+	host: "trial.ccqjtobabm3h.ap-south-1.rds.amazonaws.com", // Replace with your database hostname
+	user: "admin", // Replace with your database username
+	password: "rootroot", // Replace with your database password
+	database: "main", // Replace with your database name
 	reconnect: true,
 });
 
@@ -20,7 +20,9 @@ app.get("/doublecheck", (req, res) => {
 app.get("/user", (req, res) => {
 	const sql = "SELECT * FROM user";
 	db.query(sql, (err, data) => {
-		if (err) res.json(err + " Due To Select");
+		if (err) {
+			return res.status(500).json(err + " Due To Select");
+		}
 		return res.json(data);
 	});
 });
@@ -28,4 +30,3 @@ app.get("/user", (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
 });
-
