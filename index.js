aa5528e1d3707b9ba35796a97c66d1168b8c6fd5
Saurabh Jain <sj2100333@gmail.com>
2024-01-26 18:07:54 +0530

    for Hostinger

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
-	host: "trial.ccqjtobabm3h.ap-south-1.rds.amazonaws.com", // Replace with your database hostname
-	user: "admin", // Replace with your database username
-	password: "rootroot", // Replace with your database password
-	database: "main", // Replace with your database name
-	reconnect: true,
-});
+// const db = mysql.createConnection({
+// 	host: "trial.ccqjtobabm3h.ap-south-1.rds.amazonaws.com", // Replace with your database hostname
+// 	user: "admin", // Replace with your database username
+// 	password: "rootroot", // Replace with your database password
+// 	database: "main", // Replace with your database name
+// 	reconnect: true,
+// });
 
 app.get("/checking", (req, res) => {
 	res.status(200).json("Welcome, your app is working well");
@@ -17,15 +17,15 @@ app.get("/checking", (req, res) => {
 app.get("/doublecheck", (req, res) => {
 	res.status(200).json("Welcome, your app is working well now also");
 });
-app.get("/user", (req, res) => {
-	const sql = "SELECT * FROM user";
-	db.query(sql, (err, data) => {
-		if (err) {
-			return res.status(500).json(err + " Due To Select");
-		}
-		return res.json(data);
-	});
-});
+// app.get("/user", (req, res) => {
+// 	const sql = "SELECT * FROM user";
+// 	db.query(sql, (err, data) => {
+// 		if (err) {
+// 			return res.status(500).json(err + " Due To Select");
+// 		}
+// 		return res.json(data);
+// 	});
+// });
 
 app.listen(PORT, () => {
 	console.log(`Server running at http://localhost:${PORT}`);
